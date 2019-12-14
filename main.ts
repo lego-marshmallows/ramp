39db110dab9023eb9970857d641a91ffe0282fd4
Peli de Halleux <pelikhan@users.noreply.github.com>
2019-12-14 15:24:34 -0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,7 @@
-
+motors.mediumA.setBrake(true)
+motors.mediumA.run(10, 30, MoveUnit.Degrees)
+motors.largeBC.steer(0, 40, 2400, MoveUnit.Degrees)
+motors.stopAll()
+motors.largeBC.steer(-125, 30, 362, MoveUnit.Degrees)
+motors.largeBC.steer(0, 80, 2400, MoveUnit.Degrees)
+brick.exitProgram()
